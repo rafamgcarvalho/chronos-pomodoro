453aa5563de50bd2b904efef60f822aa9f912a41
Rafa Carvalho <rafacarvalho.10@outlook.com>
2025-07-28 19:36:08 -0300

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import './styles/theme.css';
-import './styles/global.css';
 import {Container} from './components/Container/Container';
 import { Heading } from './components/Heading/Heading';
+import {Logo} from './components/Logo/Logo';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
     return (
         <>
             <Container>
-                <Heading>LOGO</Heading>
+                <Logo></Logo>
             </Container>
 
             <Container>
