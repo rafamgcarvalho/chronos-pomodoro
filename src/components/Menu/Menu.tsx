ce334cfa306b5bb7557cf920759f25a3f07e84b6
Rafa Carvalho <rafacarvalho.10@outlook.com>
2025-07-28 20:40:34 -0300

    Menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,32 +1,24 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from 'lucide-react';
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './Menu.module.css';
 
 export function Menu() {
     return (
-        <div className={styles.menu}>
-            <div className="homePage">
-                <a className={styles.menuLink} href="#">
-                    <HouseIcon />
-                </a>
-            </div>
+        <nav className={styles.menu}>
+            <a className={styles.menuLink} href="#">
+                <HouseIcon />
+            </a>
 
-            <div className="history">
-                <a className={styles.menuLink} href="#">
-                    <HistoryIcon />
-                </a>
-            </div>
+            <a className={styles.menuLink} href="#">
+                <HistoryIcon />
+            </a>
 
-            <div className="config">
-                <a className={styles.menuLink} href="#">
-                    <SettingsIcon />
-                </a>
-            </div>
+            <a className={styles.menuLink} href="#">
+                <SettingsIcon />
+            </a>
 
-            <div className="colorTheme">
-                <a className={styles.menuLink} href="#">
-                    <SunIcon />
-                </a>
-            </div>
-        </div>
+            <a className={styles.menuLink} href="#">
+                <SunIcon />
+            </a>
+        </nav>
     );
 }
