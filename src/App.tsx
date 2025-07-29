90aa8f449200981b6f2968166fc64b1c4696622c
Rafa Carvalho <rafacarvalho.10@outlook.com>
2025-07-29 19:17:16 -0300

    Input

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import {Container} from './components/Container/Container';
 // import { Heading } from './components/Heading/Heading';
 import {Menu} from './components/Menu/Menu';
 import {Logo} from './components/Logo/Logo';
+import {CountDown} from './components/CountDown/CountDown';
+import {DefaultInput} from './components/DefaultInput/DefaultInput';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -15,6 +17,31 @@ export function App() {
 
             <Container>
                 <Menu></Menu>
+            </Container>
+
+            <Container>
+                <CountDown></CountDown>
+            </Container>
+
+            <Container>
+                <form className='form'action="">
+                    <div className="formRow">
+                        <DefaultInput id= 'meuInput' type = 'text'></DefaultInput>
+                    </div>
+
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formRow">
+                        <p>Ciclos.</p>
+                        <p>0 0 0 0 0 0 0 0 0</p>
+                    </div>
+
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     )
