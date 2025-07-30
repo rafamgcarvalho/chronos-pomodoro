4981fe733d4cc1054c9a4f82493ecfe8c79bbc8f
Rafa Carvalho <rafacarvalho.10@outlook.com>
2025-07-30 09:26:24 -0300

    finalizando componente input

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export function App() {
             <Container>
                 <form className='form'action="">
                     <div className="formRow">
-                        <DefaultInput id= 'meuInput' type = 'text'></DefaultInput>
+                        <DefaultInput labelText='Task' id= 'meuInput' type = 'text'></DefaultInput>
                     </div>
 
                     <div className="formRow">
--- src/components/DefaultInput/DefaultInput.tsx
+++ src/components/DefaultInput/DefaultInput.tsx
@@ -1,11 +1,14 @@
 type DefaultInputProps = {
     id: string;
+    labelText?: string;
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({id, type}: DefaultInputProps) {
+export function DefaultInput({id, type, labelText}: DefaultInputProps) {
     return (
         <>
-        <label htmlFor= {id}>task</label>
+        {
+            labelText && <label htmlFor= {id}>{labelText}</label>
+        }
         <input id= {id} type={type} />
         </>
     );
