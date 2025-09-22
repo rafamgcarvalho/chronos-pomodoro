e36dc2a05b66c0affd688598e027e589a0852fbb
Rafa Carvalho <rafacarvalho.10@outlook.com>
2025-09-22 19:05:12 -0300

    formatando os segundos

--- src/components/MainForm/Index.tsx
+++ src/components/MainForm/Index.tsx
@@ -2,19 +2,69 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles/Index";
 import { DefaultButton } from "../DefaultButton/DefaultButton";
 import { DefaultInput } from "../DefaultInput/DefaultInput";
-import { useState } from "react";
+import { useRef} from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/UseTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
-    const [taskName, setTaskName] = useState('');
+    const { state, setState } = useTaskContext();
+    const taskNameInput = useRef<HTMLInputElement>(null);
+
+    //ciclos
+    const nextCycle = getNextCycle(state.currentCycle);
+    const nextCycleType = getNextCycleType(nextCycle)
 
     function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
+
+        if (taskNameInput.current === null) return;
+
+        const taskName = taskNameInput.current.value.trim();
+
+        if (!taskName) {
+            alert('Digite o nome da tarefa');
+
+            return;
+        }
+
+        const newTask: TaskModel = {
+            id: Date.now().toString(),
+            name: taskName,
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            duration: state.config[nextCycleType],
+            type: nextCycleType,
+        };
+
+        const secondsRemaining = newTask.duration * 60;
+
+        setState(prevState => {
+            return {
+                ...prevState,
+                config: {...prevState.config},
+                activeTask: newTask,
+                currentCycle: nextCycle,
+                secondsRemaining,
+                formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+                tasks: [...prevState.tasks, newTask],
+            }
+        })
     }
 
     return (
         <form onSubmit={handleCreateNewTask} className='form' action="">
             <div className="formRow">
-                <DefaultInput labelText='Task' id='meuInput' type='text' placeholder='Digite Algo' value={taskName} onChange={e => setTaskName(e.target.value)}></DefaultInput>
+                <DefaultInput 
+                    labelText='Task' 
+                    id='meuInput' 
+                    type='text' 
+                    placeholder='Digite Algo' 
+                    ref={taskNameInput}>
+                </DefaultInput>
             </div>
 
             <div className="formRow">
